6ea9c4cc3b964e503e3a887b9066061adb0c465a
Matthias Hölzl <tc@xantira.com>
2024-03-22 00:30:05 +0100

    Update der GRASP Beispiele

--- Python/CodeAlong/Woche-4/09 GRASP Beispiele.py
+++ Python/CodeAlong/Woche-4/09 GRASP Beispiele.py
@@ -25,10 +25,105 @@
 
 # %% [markdown] lang="de" tags=["subslide"] slideshow={"slide_type": "subslide"}
 #
-# ## Beispiel: Adapter-Pattern
+# ## Problem: Anbindung an externe Bibliotheken
 #
-# Wie können wir das Adapter-Pattern mit den abstrakten GRASP-Patterns
-# analysieren?
+# - Wir haben externe Bibliotheken, die wir in unserem Programm verwenden wollen.
+# - Leider sind diese plattformspezifisch.
+# - Wie wollen unsere Anwendung so schreiben, dass wir die Bibliothek austauschen
+#   können, ohne die Anwendung ändern zu müssen.
+
+# %% tags=["keep"]
+class ExternalLibraryPC:
+    @staticmethod
+    def specific_request(n: int, text: str) -> str:
+        return f"ExternalLibraryPC: {text * n}"
+
+
+# %% tags=["keep"]
+class ExternalLibraryMac:
+    @staticmethod
+    def specific_request(text: str, n: int, sep: str = "") -> str:
+        return f"ExternalLibraryMac: {(text + sep) * n}"
+
+
+# %% [markdown] lang="de"
+#
+# Wie können wir das Problem mit den GRASP-Patterns lösen?
+
+# %% [markdown] lang="de" tags=["subslide"] slideshow={"slide_type": "subslide"}
+#
+# ### Protected Variations
+#
+# - Wir können die Anbindung an die externe Bibliothek als eine geschützte
+#   Variation betrachten.
+# - Der Variationspunkt ist der Aufruf der Funktion, die den String erzeugt.
+# - Alle anderen Teile unserer Anwendung sollen vor Änderungen geschützt sein.
+
+# %% [markdown] lang="de" tags=["subslide"] slideshow={"slide_type": "subslide"}
+#
+# ### Indirection/Polymorphie
+#
+# - Welchen Mechanismus können wir dafür verwenden?
+# - Wir führen eine Indirektion ein, und zwar konkret ein Interface, das die
+#   gewünschte Funktionalität abstrahiert.
+# - Dieses Interface ist eine Pure Fabrication
+
+
+# %% tags=["keep", "subslide"] slideshow={"slide_type": "subslide"}
+from abc import ABC, abstractmethod
+
+
+# %% tags=["keep"]
+class LibraryAdapter(ABC):
+    @abstractmethod
+    def process_string(self, text: str, sep: str, n: int) -> str: ...
+
+
+# %% [markdown] lang="de" tags=["subslide"] slideshow={"slide_type": "subslide"}
+#
+# Wir verwenden das Interface in unserer Anwendung:
+
+# %% tags=["keep"]
+class Application:
+    def __init__(self, adapter: LibraryAdapter):
+        self.adapter = adapter
+
+    def process(self, text: str) -> str:
+        return self.adapter.process_string(text, "! ", 3)
+
+
+# %% [markdown] lang="de" tags=["subslide"] slideshow={"slide_type": "subslide"}
+#
+# Wir implementieren das Interface für jede Variation, die wir unterstützen wollen:
+
+# %% tags=["keep"]
+class PcAdapter(LibraryAdapter):
+    def process_string(self, text: str, sep: str, n: int) -> str:
+        return ExternalLibraryPC.specific_request(n, text + sep)
+
+
+# %% tags=["keep"]
+class MacAdapter(LibraryAdapter):
+    def process_string(self, text: str, sep: str, n: int) -> str:
+        return ExternalLibraryMac.specific_request(text, n, sep)
+
+
+# %% [markdown] lang="de" tags=["subslide"] slideshow={"slide_type": "subslide"}
+#
+# Jetzt können wir den Variationspunkt in unserer Anwendung austauschen:
+
+# %% tags=["keep"]
+app = Application(PcAdapter())
+print(app.process("Go"))
+
+# %% tags=["keep"]
+app = Application(MacAdapter())
+print(app.process("Stop"))
+
+
+# %% [markdown] lang="de" tags=["subslide"] slideshow={"slide_type": "subslide"}
+#
+# ## Beispiel: Adapter-Pattern
 #
 # <img src="img/pat_adapter.svg"
 #      style="display: block; margin: auto; width: 80%;"/>
